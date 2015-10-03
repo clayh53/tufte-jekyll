28f083989e38d796d1911d4c67b3aa200c768d2c
clayh53 <clay.harmon.50@gmail.com>
2015-10-02 20:51:59 -0400

    fixed layout issues with mathjax tag by adding zero space html entity ahead of script

--- _plugins/mathjaxtag.rb
+++ _plugins/mathjaxtag.rb
@@ -1,22 +1,27 @@
-module Jekyll
+mmodule Jekyll
   class MathJaxBlockTag < Liquid::Tag
     def render(context)
-      '<script type="math/tex; mode=display">'
+      '&#8203;<script type="math/tex; mode=display">'
     end
   end
 class MathJaxInlineTag < Liquid::Tag
     def render(context)
-      '<script type="math/tex">'
+      '&#8203;<script type="math/tex">'
     end
   end
-class MathJaxEndTag < Liquid::Tag
+class MathJaxEndBlockTag < Liquid::Tag
     def render(context)
       '</script>'
     end
   end
+class MathJaxEndInlineTag < Liquid::Tag
+    def render(context)
+      '</script>'
+    end
+  end  
 end
  
 Liquid::Template.register_tag('math', Jekyll::MathJaxBlockTag)
 Liquid::Template.register_tag('m', Jekyll::MathJaxInlineTag)
-Liquid::Template.register_tag('endmath', Jekyll::MathJaxEndTag)
-Liquid::Template.register_tag('em', Jekyll::MathJaxEndTag)
+Liquid::Template.register_tag('endmath', Jekyll::MathJaxEndBlockTag)
+Liquid::Template.register_tag('em', Jekyll::MathJaxEndInlineTag)
